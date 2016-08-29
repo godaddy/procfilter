38bfb2ca2918bb7482ea415d52f77c475efd3728
Emerson R. Wiley <emerson.wiley@gmail.com>
2016-08-29 12:05:21 -0700

    Update version number to 1.0.0-beta.3

--- testlua/testlua.lua
+++ testlua/testlua.lua
@@ -2,7 +2,7 @@
 function ProcFilterEvent(e)
   procfilter.Log("Entering event handler")
   if e.dwEventId == procfilter.PROCFILTER_EVENT_INIT then
-    procfilter.RegisterPlugin("1.0.0-beta.2", "luatest", 0, 0, true, procfilter.PROCFILTER_EVENT_PROCESS_CREATE, procfilter.PROCFILTER_EVENT_NONE)
+    procfilter.RegisterPlugin("1.0.0-beta.3", "luatest", 0, 0, true, procfilter.PROCFILTER_EVENT_PROCESS_CREATE, procfilter.PROCFILTER_EVENT_NONE)
   elseif e.dwEventId == procfilter.PROCFILTER_EVENT_PROCESS_CREATE then
     -- This fails; the file name is Unicode and Lua strings are ASCII.
     -- SWIG doesn't support this by default so it'll have to be added later.
