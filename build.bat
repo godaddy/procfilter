7aba31e043f8a873fdc21c8aef5b5be718f79f96
Emerson R. Wiley <emerson.wiley@gmail.com>
2016-09-01 12:43:47 -0700

    Fix extension typo and convert to DOS format

--- build.bat
+++ build.bat
@@ -3,4 +3,4 @@ msbuild.exe /p:configuration="Debug" /p:platform="x64" procfilter.sln
 msbuild.exe /p:configuration="Release" /p:platform="Win32" procfilter.sln
 msbuild.exe /p:configuration="Release" /p:platform="x64" procfilter.sln
 
-sign_binaries.bat & build_installers.bat & sign_installers.ba
+sign_binaries.bat & build_installers.bat & sign_installers.bat
