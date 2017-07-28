feff2ef881583049d96c05ee6d260d62f317870c
Emerson R. Wiley <emerson.wiley@gmail.com>
2017-07-27 23:05:05 -0700

    driver/kmdriver.cpp
    Ignore system thread events and allow blocking from DLL load events

--- service/hash.cpp
+++ service/hash.cpp
@@ -23,7 +23,7 @@
 // DEALINGS IN THE SOFTWARE.
 //
 
-#include "sha1.hpp"
+#include "hash.hpp"
 
 #include "strlcat.hpp"
 
@@ -32,16 +32,18 @@
 
 
 bool
-Sha1File(const WCHAR *lpszFileName, char o_hexdigest[SHA1_HEXDIGEST_LENGTH+1], unsigned char o_rawdigest[SHA1_DIGEST_SIZE])
+HashFile(const WCHAR *lpszFileName, HASHES *hashes)
 {
+	ZeroMemory(hashes, sizeof(HASHES));
+
 	bool rv = false;
 	BOOL rc = FALSE;
 	HCRYPTPROV hCryptoProvider = NULL;
-	HCRYPTHASH hHash = NULL;
+	HCRYPTHASH hMd5Hash = NULL;
+	HCRYPTHASH hSha1Hash = NULL;
+	HCRYPTHASH hSha256Hash = NULL;
 	DWORD dwBytesRead = 0;
 	BYTE baBlock[BLOCK_SIZE];
-	BYTE baDigest[SHA1_DIGEST_SIZE];
-	DWORD dwDigestLen = sizeof(baDigest);
 
 	// Open the input file for reading
 	HANDLE hFile = CreateFile(lpszFileName, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, 0, NULL);
@@ -49,35 +51,47 @@ Sha1File(const WCHAR *lpszFileName, char o_hexdigest[SHA1_HEXDIGEST_LENGTH+1], u
 
 	// Open up the Wincrypt API
 	if (!CryptAcquireContext(&hCryptoProvider, NULL, NULL, PROV_RSA_FULL, CRYPT_VERIFYCONTEXT | CRYPT_SILENT)) goto cleanup;
-	if (!CryptCreateHash(hCryptoProvider, CALG_SHA1, 0, 0, &hHash)) goto cleanup;
+	if (!CryptCreateHash(hCryptoProvider, CALG_MD5, 0, 0, &hMd5Hash)) goto cleanup;
+	if (!CryptCreateHash(hCryptoProvider, CALG_SHA1, 0, 0, &hSha1Hash)) goto cleanup;
+	if (!CryptCreateHash(hCryptoProvider, CALG_SHA_256, 0, 0, &hSha256Hash)) goto cleanup;
 
 	// Loop and read all bytes in the file and pass them through the hash context
 	while ((rc = ReadFile(hFile, baBlock, sizeof(baBlock), &dwBytesRead, NULL)) == TRUE) {
 		if (dwBytesRead == 0) break;
-		if (!CryptHashData(hHash, baBlock, dwBytesRead, 0)) goto cleanup;
+		if (!CryptHashData(hMd5Hash, baBlock, dwBytesRead, 0)) goto cleanup;
+		if (!CryptHashData(hSha1Hash, baBlock, dwBytesRead, 0)) goto cleanup;
+		if (!CryptHashData(hSha256Hash, baBlock, dwBytesRead, 0)) goto cleanup;
 	}
 
 	if (!rc) goto cleanup;
 
-	// Extract the hash value
-	if (!CryptGetHashParam(hHash, HP_HASHVAL, baDigest, &dwDigestLen, 0)) goto cleanup;
+	// Extract the hash values
+	DWORD dwDigestLen = sizeof(hashes->md5_digest);
+	if (!CryptGetHashParam(hMd5Hash, HP_HASHVAL, hashes->md5_digest, &dwDigestLen, 0)) goto cleanup;
 
-	// Convert and store the digest if requested
-	if (o_hexdigest) {
-		for (size_t i = 0; i < SHA1_DIGEST_SIZE; ++i) {
-			strlprintf(&o_hexdigest[i*2], 3, "%.02X", baDigest[i]);
-		}
+	dwDigestLen = sizeof(hashes->sha1_digest);
+	if (!CryptGetHashParam(hSha1Hash, HP_HASHVAL, hashes->sha1_digest, &dwDigestLen, 0)) goto cleanup;
+
+	dwDigestLen = sizeof(hashes->sha256_digest);
+	if (!CryptGetHashParam(hSha256Hash, HP_HASHVAL, hashes->sha256_digest, &dwDigestLen, 0)) goto cleanup;
+
+	// Convert and store the digest to hex
+	for (size_t i = 0; i < MD5_DIGEST_SIZE; ++i) {
+		strlprintf(&hashes->md5_hexdigest[i*2], 3, "%.02X", hashes->md5_digest[i]);
 	}
-
-	// Store the raw digest if requested
-	if (o_rawdigest) {
-		memcpy(o_rawdigest, baDigest, SHA1_DIGEST_SIZE);
+	for (size_t i = 0; i < SHA1_DIGEST_SIZE; ++i) {
+		strlprintf(&hashes->sha1_hexdigest[i*2], 3, "%.02X", hashes->sha1_digest[i]);
+	}
+	for (size_t i = 0; i < SHA256_DIGEST_SIZE; ++i) {
+		strlprintf(&hashes->sha256_hexdigest[i*2], 3, "%.02X", hashes->sha256_digest[i]);
 	}
 
 	rv = true;
 
 cleanup:
-	if (hHash) CryptDestroyHash(hHash);
+	if (hSha256Hash) CryptDestroyHash(hSha256Hash);
+	if (hSha1Hash) CryptDestroyHash(hSha1Hash);
+	if (hMd5Hash) CryptDestroyHash(hMd5Hash);
 	if (hCryptoProvider) CryptReleaseContext(hCryptoProvider, 0);
 	if (hFile != INVALID_HANDLE_VALUE) CloseHandle(hFile);
 
--- service/hash.hpp
+++ service/hash.hpp
@@ -30,7 +30,7 @@
 #include "procfilter/procfilter.h"
 
 //
-// Perform a SHA1 hash of a file
+// Get cryptographic hashes of a file
 //
-bool Sha1File(const WCHAR *lpszFileName, char o_hexdigest[SHA1_HEXDIGEST_LENGTH+1], BYTE o_rawdigest[SHA1_DIGEST_SIZE]);
+bool HashFile(const WCHAR *lpszFileName, HASHES *hashes);
 
